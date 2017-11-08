6194a08da16465f0ed34b9bd90248386a33fd721
Jesus Almaraz <almarazj@ie.ibm.com>
2017-11-08 11:11:20 +0100

    removing Spring Cloud/Netflix

--- src/main/java/com/ibm/microservices/wfd/MenuApplicationUI.java
+++ src/main/java/com/ibm/microservices/wfd/MenuApplicationUI.java
@@ -2,15 +2,10 @@ package com.ibm.microservices.wfd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableCircuitBreaker
 public class MenuApplicationUI {
 
     public static void main(String[] args) {
@@ -18,7 +13,6 @@ public class MenuApplicationUI {
     }
 
     @Bean
-    @LoadBalanced
     public RestTemplate restTemplate() {
       return new RestTemplate();
     }
--- src/main/java/com/ibm/microservices/wfd/controller/MenuController.java
+++ src/main/java/com/ibm/microservices/wfd/controller/MenuController.java
@@ -16,12 +16,6 @@ import org.springframework.boot.json.BasicJsonParser;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
-import org.springframework.cloud.sleuth.Span;
-import org.springframework.cloud.sleuth.SpanAccessor;
-
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -35,9 +29,6 @@ public class MenuController {
 
   @Autowired
   private RestTemplate restTemplate;
-
-  @Autowired
-  private SpanAccessor spanAccessor;
 
   @Value("${wfd.menu.url}")
   private String menu;
@@ -61,48 +52,10 @@ public class MenuController {
     List<String> dessertOptions = (List<String>)((Map<String, Object>)jsonMap.get("desserts")).get("menu");
     model.addAttribute("dessertOptions", dessertOptions);
 
-    Span span = this.spanAccessor.getCurrentSpan();
-    String traceId = "UNSET";
-    if(span != null){
-      traceId = Span.idToHex(span.getTraceId());
-    }
-    model.addAttribute("TraceId", traceId);
-
     return "index";
   }
-  @HystrixCommand(fallbackMethod="getDefaultMenu", groupKey="WhatsForDinnerUI", commandKey="GetMenuForUI")
   public String getMenu(){
     return this.restTemplate.getForObject(this.menu, String.class);
   }
-
-  public String getDefaultMenu(){
-    return "{entrees:{menu:[Hamburger,Hot Dog,Spaghetti]},desserts:{menu:[Cookies,Candy,Cake]},appetizers:{menu:[Chips,Salsa,Bruschetta]}}";
-  }
-
-  /*public String getDefaultMenu(Model model){
-
-    List<String> appetizerList = new ArrayList<String>();
-                 appetizerList.add("Chips");
-                 appetizerList.add("Salsa");
-                 appetizerList.add("Bruschetta");
-
-    model.addAttribute("appetizerOptions", appetizerList);
-
-    List<String> entreeList = new ArrayList<String>();
-                 entreeList.add("Hamburger");
-                 entreeList.add("Hot Dog");
-                 entreeList.add("Spaghetti");
-
-    model.addAttribute("entreeOptions", entreeList);
-
-    List<String> dessertList = new ArrayList<String>();
-                 dessertList.add("Cookies");
-                 dessertList.add("Candy");
-                 dessertList.add("Cake");
-
-    model.addAttribute("dessertOptions", dessertList);
-
-    return "index";
-  }*/
 
 }

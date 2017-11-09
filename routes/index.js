f357ccf7f68f9df5e04af3604883fe3b304dd511
Hemankita Perabathini <hemankita.perabathini@ibm.com>
2017-11-09 15:16:23 -0600

    Modifying index.js

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,9 @@ var api = require('../api');
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
-  //call the api apiGet and create callback function
   api.apiGet(function (data) {
-    // render to the index.jade and pass the data from api call
-    res.render('index', { result : data});
+    res.render('index', { title: 'What\'s for Dinner? - IBM Cloud Architecture - MicroProfile Reference Application', { result : data});
   });
 });
 

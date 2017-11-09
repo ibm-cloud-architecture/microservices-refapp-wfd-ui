81a52051584c8515dd0628c99aa5a80ce0687d44
Hemankita Perabathini <hemankita.perabathini@ibm.com>
2017-11-09 15:32:08 -0600

    Adding title to webpage

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 router.get('/', function(req, res) {
   api.apiGet(function (data) {
-    res.render('index', { title: 'What\'s for Dinner? - IBM Cloud Architecture - MicroProfile Reference Application', { result : data});
+    res.render('index', { title: 'What\'s for Dinner? - IBM Cloud Architecture - MicroProfile Reference Application', result : data});
   });
 });
 

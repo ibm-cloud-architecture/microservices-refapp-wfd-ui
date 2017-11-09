ddc51f8102a2c5ed73727fc4e220e97bacb2ae9d
Hemankita Perabathini <hemankita.perabathini@ibm.com>
2017-11-09 00:21:11 -0600

    removing angular project

--- WfdMenuUI/src/app/menu/desserts.ts
+++
@@ -1,7 +0,0 @@
-export class Desserts {
-  constructor(
-  public order: number,
-  public menu: string[],
-  public type: string
-  ) {}
-}
--- WfdMenuUI/src/app/menu/entrees.ts
+++
@@ -1,7 +0,0 @@
-export class Entrees {
-  constructor(
-  public order: number,
-  public menu: string[],
-  public type: string
-  ) {}
-}
